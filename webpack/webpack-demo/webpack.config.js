861ed8e73ec0122e798a42f10b56441616bd3255
qiaogaojian <qiaogaojian@vip.qq.com>
2021-04-13 13:30:19 +0800

    optimize webpack config add build mode and stop generate LICENSE file

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false, //不将注释提取到单独的文件中
+      }),
+    ],
   },
 };
