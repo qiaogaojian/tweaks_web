c46d859113a4d1c076cd81a2e1306f7687fed225
qiaogaojian <qiaogaojian@vip.qq.com>
2021-04-13 11:32:53 +0800

    init project and test pure javascript dependencies
    npm init -y
    npm install webpack webpack-cli --save-dev
    
    There are problems with managing JavaScript projects this way:
    - It is not immediately apparent that the script depends on an external library.
    - If a dependency is missing, or included in the wrong order, the application will not function properly.
    - If a dependency is included but not used, the browser will be forced to download unnecessary code.

---
+++ webpack/webpack-demo/src/index.js
@@ -0,0 +1,7 @@
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = _.join(["hello", "webpack"], "");
+  return element;
+}
+
+document.body.appendChild(component());
