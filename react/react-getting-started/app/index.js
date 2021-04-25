3768543ceae578ba6c45b5100fa3602c16a528de
qiaogaojian <qiaogaojian@vip.qq.com>
2021-04-25 14:53:03 +0800

    feat: add first example

--- react/react-getting-started/app/index.js
+++ react/react-getting-started/app/index.js
@@ -3,6 +3,10 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Hi() {
+  return <div>Hello World!</div>;
+}
+
 function Hi0(props) {
   return (
     <div>
