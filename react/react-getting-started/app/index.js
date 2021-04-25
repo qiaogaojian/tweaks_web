a1b8154867af666ca3809520e6a085a61ef6b61e
qiaogaojian <qiaogaojian@vip.qq.com>
2021-04-25 11:30:26 +0800

    feat: react组件的匿名函数写法

--- react/react-getting-started/app/index.js
+++ react/react-getting-started/app/index.js
@@ -2,12 +2,26 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Hi({firstName,lastName}) {
+// function Hi({firstName,lastName}) {
+//   return (
+//     <div>
+//       Hello <strong>{firstName} {lastName}!</strong>
+//     </div>
+//   );
+// }
+
+const Hi = ({ firstName, lastName }) => {
   return (
     <div>
-      Hello <strong>{firstName} {lastName}!</strong>
+      Hello{" "}
+      <strong>
+        {firstName} {lastName}!
+      </strong>
     </div>
   );
-}
+};
 
-ReactDOM.render(<Hi firstName="Michael" lastName="Qiao" />, document.querySelector("#root"));
+ReactDOM.render(
+  <Hi firstName="Michael" lastName="Qiao" />,
+  document.querySelector("#root")
+);
