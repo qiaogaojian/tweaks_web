9183d70ba0a017cdb83ee38d759f3c0a1fdd43d1
qiaogaojian <qiaogaojian@vip.qq.com>
2021-04-26 16:22:39 +0800

    refractor: 重构代码提高可读性

--- react/react-getting-started/app/hook.jsx
+++ react/react-getting-started/app/hook.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 const OneTimeButton = (props) => {
   const [clickState, setClickState] = React.useState(false);
+
+  const doClick = () => {
+    setClickState(true);
+    props.onClick();
+  };
+
   return (
-    <button
-      onClick={() => {
-        setClickState(true);
-        props.onClick();
-      }}
-      disabled={clickState}
-    >
+    <button onClick={clickState ? undefined : doClick} disabled={clickState}>
       You Can Only Click Me Once.
     </button>
   );
