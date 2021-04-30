596d6b68ec525829a3fcb4b08acd2fd0494d29a4
qiaogaojian <qiaogaojian@vip.qq.com>
2021-04-30 17:22:17 +0800

    refrator: update react frame

--- react/frame/app/index.js
+++ react/frame/app/index.js
@@ -1,6 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./component/App.jsx";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import { App } from "./page/app.jsx";
+import * as Action from "./consts/actions";
+
+const initState = {
+  count: 0,
+};
+const store = createStore(reducer, applyMiddleware(thunk));
+
+function reducer(state = initState, action) {
+  switch (action.type) {
+    case Action.INCREMENT:
+      state = { count: state.count + 1 };
+      break;
+    case Action.DECREMENT:
+      state = { count: state.count - 1 };
+      break;
+    case Action.RESET:
+      state = { count: 0 };
+      break;
+  }
+  console.log("reducer", state, action);
+  return state;
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
