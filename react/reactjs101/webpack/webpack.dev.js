a542f8a12ab2bb55750320509537d7ee1b71a07e
qiaogaojian <qiaogaojian@vip.qq.com>
2021-05-24 17:03:13 +0800

    feat: 配置 ES6

--- react/reactjs101/webpack/webpack.dev.js
+++ react/reactjs101/webpack/webpack.dev.js
@@ -51,7 +51,8 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["es2015", "react"],
+            presets: ["es2015", "react", "stage-0"],
+            plugins: ["babel-plugin-transform-runtime"]
           },
         },
       },
