49f47d4d13065938cfb450b3e05b835092c1def7
qiaogaojian <qiaogaojian@vip.qq.com>
2021-05-24 14:47:29 +0800

    feat: test sample router

--- react/reactjs101/app/page/app.jsx
+++ react/reactjs101/app/page/app.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  HashRouter,
+} from "react-router-dom";
+
 import Counter from "../component/counter/counter.jsx";
+import Hello from "../component/hello/Hello.jsx";
 import "./app.css";
 
 export default class App extends React.Component {
@@ -9,9 +18,14 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="app">
-        <Counter />
-      </div>
+      <HashRouter>
+        <div className="app">
+          <Switch>
+            <Route path="/hello" component={Hello} />
+            <Route path="/counter" component={Counter} />
+          </Switch>
+        </div>
+      </HashRouter>
     );
   }
 }
